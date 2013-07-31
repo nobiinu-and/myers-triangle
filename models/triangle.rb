9ae73021dd1395363e27b7470c8d931f1566feae
Toshiyuki Ando <andou@mxp.nes.nec.co.jp>
2013-07-31 20:30:41 +0900

    unify conditional expression

--- models/triangle.rb
+++ models/triangle.rb
@@ -9,8 +9,7 @@ class Triangle
     remains = all.reject {|x| x == longest_side}
 
     return "三角形でない" if remains.size == 2 && remains[0] + remains[1] <= longest_side
-
-    return "正三角形"     if side1 == side2 && side2 == side3
+    return "正三角形"     if remains.size == 0
     return "二等辺三角形" if remains.size == 1
     "不等辺三角形"
   end
